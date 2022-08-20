7c71beebc2e140bed580b747c3a073568b2bee87
reinatt <reinattwijaya@kaist.ac.kr>
2022-08-21 00:05:06 +0900

    frontend

--- frontend/src/API/user.ts
+++ frontend/src/API/user.ts
@@ -1,98 +1,108 @@
-import { AxiosResponse } from 'axios'
-import {
-  Endpoint, AuthEndpoint, UNIVERSAL_TOKEN
-} from './base'
+import { AxiosResponse } from "axios";
+import { Endpoint, AuthEndpoint, UNIVERSAL_TOKEN } from "./base";
 
-
-export interface Transaction{
-  to            : string, 
-  from          : string, 
-  date_created  : Date, 
-  token         : string
+export interface Transaction {
+  to: string;
+  from: string;
+  date_created: Date;
+  token: string;
 }
 
-abstract class UserBase{
-  static login    : Function
-  static register : Function
-  static loadFromLocal : Function
-  static saveKey  = 'ngo-chain'
-  abstract wallet() : Promise<Array<Transaction>>
-  abstract transaction(tokenId : string) : Promise<Transaction>
-  abstract saveToLocal() : void
+abstract class UserBase {
+  static login: Function;
+  static register: Function;
+  static loadFromLocal: Function;
+  static saveKey = "ngo-chain";
+  abstract wallet(): Promise<Array<Transaction>>;
+  abstract transaction(tokenId: string): Promise<Transaction>;
+  abstract saveToLocal(): void;
 }
 
-export default class User extends UserBase{
+export default class User extends UserBase {
+  publicKey: string;
+  constructor(publicKey: string) {
+    super();
+    this.publicKey = publicKey;
+    this.saveToLocal();
+  }
   saveToLocal = () => {
-    let token   = JSON.stringify(UNIVERSAL_TOKEN)
-    let saveStr = Buffer.from(token).toString('base64')
-    localStorage.setItem(User.saveKey, saveStr)
-  }
-  wallet = () : Promise<Array<Transaction>> => {
-    let endpoint = new AuthEndpoint(
-      'get', 'user/wallet', UNIVERSAL_TOKEN
-    )
+    let payload = { publicKey: this.publicKey, token: UNIVERSAL_TOKEN };
+    let token = JSON.stringify(payload);
+    let saveStr = Buffer.from(token).toString("base64");
+    localStorage.setItem(User.saveKey, saveStr);
+  };
+  wallet = (): Promise<Array<Transaction>> => {
+    let endpoint = new AuthEndpoint("get", "user/wallet", UNIVERSAL_TOKEN);
     return new Promise((res, rej) => {
-      endpoint.req()
-      .then((resp) => res(resp.data))
-      .catch((err) => rej(err))
-    })
-  }
-  transaction = (tokenId : string) : Promise<Transaction> => {
+      endpoint
+        .req()
+        .then((resp) => res(resp.data))
+        .catch((err) => rej(err));
+    });
+  };
+  transaction = (tokenId: string): Promise<Transaction> => {
     let endpoint = new AuthEndpoint(
-      'get', `user/wallet/${tokenId}`, UNIVERSAL_TOKEN
-    )
+      "get",
+      `user/wallet/${tokenId}`,
+      UNIVERSAL_TOKEN
+    );
     return new Promise((res, rej) => {
-      endpoint.req()
-      .then((resp) => res(resp.data))
-      .catch((err) => rej(err))
-    })
-  }
-  checkLogin = () : Promise<AxiosResponse> => {
-    let endpoint = new AuthEndpoint('get', `user/check/`, UNIVERSAL_TOKEN)
-    return endpoint.req()
-  }
+      endpoint
+        .req()
+        .then((resp) => res(resp.data))
+        .catch((err) => rej(err));
+    });
+  };
+  checkLogin = (): Promise<AxiosResponse> => {
+    let endpoint = new AuthEndpoint("get", `user/checklogin/`, UNIVERSAL_TOKEN);
+    return endpoint.req();
+  };
 
-  static login = (
-    username : string, password : string
-  ) : Promise<User> => {
+  static login = (username: string, password: string): Promise<User> => {
     let payload = {
-      username : username, password : password
-    }
-    let endpoint = new Endpoint('post', 'user/login/')
+      username: username,
+      password: password,
+    };
+    let endpoint = new Endpoint("post", "user/login/");
     return new Promise((res, rej) => {
-      endpoint.req(payload)
-      .then((resp) => {
-        UNIVERSAL_TOKEN.token = resp.data.token
-        res(new User())
-      })
-      .catch((err) => rej(err))
-    })
-  }
+      endpoint
+        .req(payload)
+        .then((resp) => {
+          UNIVERSAL_TOKEN.token = resp.data.token;
+          res(new User(resp.data.public_key));
+        })
+        .catch((err) => rej(err));
+    });
+  };
   static register = (
-    username : string, email : string, 
-    password : string, public_key : string
-  ) : Promise<User> => {
+    username: string,
+    email: string,
+    password: string,
+    public_key: string
+  ): Promise<User> => {
     let payload = {
-      username  : username, 
-      email     : email, 
-      password  : password, 
-      public_key: public_key
-    }
-    let endpoint = new Endpoint('post', 'user/register/')
+      username: username,
+      email: email,
+      password: password,
+      public_key: public_key,
+    };
+    let endpoint = new Endpoint("post", "user/register/");
     return new Promise((res, rej) => {
-      endpoint.req(payload)
-      .then((resp) => {
-        UNIVERSAL_TOKEN.token = resp.data.token
-        res(new User())
-      })
-      .catch((err) => rej(err))
-    })
-  }
-  static loadFromLocal = () : User => {
-    let userToken = localStorage.getItem(User.saveKey)
-    let userJson  = Buffer.from(userToken, 'base64').toString('utf-8')
-    let userTok   = JSON.parse(userJson)
-    UNIVERSAL_TOKEN.token = userTok.token
-    return new User()
-  }
+      endpoint
+        .req(payload)
+        .then((resp) => {
+          UNIVERSAL_TOKEN.token = resp.data.token;
+          res(new User());
+        })
+        .catch((err) => rej(err));
+    });
+  };
+  static loadFromLocal = (): User => {
+    let userToken = localStorage.getItem(User.saveKey);
+    let userJson = Buffer.from(userToken, "base64").toString("utf-8");
+    let userTok = JSON.parse(userJson);
+    UNIVERSAL_TOKEN.token = userTok.token.token;
+    let publicKey = userTok.publicKey;
+    return new User(publicKey);
+  };
 }
